1e5c76b8a0ac2db5b949d331aa4e222119f7b167
Yanjun <shiyanjun@yeah.net>
2014-01-25 18:10:40 +0800

    Add test case for multi-threaded PUSH.

--- src/test/java/org/shirdrn/activemq/component/TestDefaultActiveMQProducer.java
+++ src/test/java/org/shirdrn/activemq/component/TestDefaultActiveMQProducer.java
@@ -1,5 +1,8 @@
 package org.shirdrn.activemq.component;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import javax.jms.JMSException;
 
 import org.apache.commons.logging.Log;
@@ -22,17 +25,55 @@ public class TestDefaultActiveMQProducer {
 	public void initialize() {
 		ContextReadable ctx = new PropertiesConfiguration("activemq.properties");
 		context = new DefaultActiveMQContext(ctx);
-		activeMQProducer = new DefaultActiveMQProducer(context);
+		activeMQProducer = this.newProducer();
+	}
+	
+	private ActiveMQProducer newProducer() {
+		return new DefaultActiveMQProducer(context);
 	}
 	
 	@Test
 	public void push() throws JMSException, InterruptedException {
 		activeMQProducer.establish();
-		for (int i = 950; i < 1000; i++) {
+		for (int i = 1000; i < 1500; i++) {
 			String message = "xxxxxxxxxxx-" + i;
 			activeMQProducer.push(message);
 			LOG.info("Push message: " + message);
 			Thread.sleep(100);
 		}
+	}
+	
+	@Test
+	public void multiThreadedPush() throws JMSException, InterruptedException {
+		int workers = 3;
+		ActiveMQProducer[] producers = new ActiveMQProducer[workers];
+		for (int i = 0; i < producers.length; i++) {
+			producers[i] = newProducer();
+			producers[i].establish();
+		}
+		ExecutorService pool = Executors.newFixedThreadPool(workers);
+		
+		for (int i = 0; i < 1500; i++) {
+			final ActiveMQProducer producer = getProducer(i, producers);
+			final String message = "xxxxxxxxxxx-" + i;
+			pool.execute(new Runnable() {
+
+				@Override
+				public void run() {
+					producer.push(message);
+					LOG.info(Thread.currentThread().getId() + ": Push message: " + message);
+				}
+				
+			});
+		}
+		synchronized(pool) {
+			pool.wait();
+		}
+	}
+	
+	private ActiveMQProducer getProducer(int id, ActiveMQProducer[] producers) {
+		int workers = producers.length;
+		int index = id % workers;
+		return producers[index];
 	}
 }
